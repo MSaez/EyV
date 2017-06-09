2f26419158df864d49ce82483c0db6757d90ad8a
Marcelo Sáez <marcelo.saez.t@gmail.com>
2017-06-09 12:04:14 -0400

    Mostrar Folio de Presupuesto en Listar presupuestos

--- views/presupuesto/index.php
+++ views/presupuesto/index.php
@@ -30,6 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel, 
         'columns' => [
             //['class' => 'yii\grid\SerialColumn'],
+            'OT_ID',
             [
                 'attribute' => 'VEH_ID', 
                 'value' => 'vEH.VEH_PATENTE',
